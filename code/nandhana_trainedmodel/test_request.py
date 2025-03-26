38a7b0d286d5a548c04be91c93e1c57dd626d1e7
C B Nandhana <146117634+nandhana04cb@users.noreply.github.com>
2025-03-26 21:22:35 +0530

    Delete code/nandhana_trainedmodel directory

--- code/nandhana_trainedmodel/test_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/classify"
-data = {"sms": "Your account was debited with INR 5000."}
-
-response = requests.post(url, json=data)
-
-print("Status Code:", response.status_code)
-print("Response:", response.json())
