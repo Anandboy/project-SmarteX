d50fa1177f9c5ed6548df0457f1b1df08f9736fe
Swarna-N-T <146638999+Swarna-N-T@users.noreply.github.com>
2025-03-26 22:29:25 +0530

    Update auth_service.dart

--- code/swarna/services/auth_service.dart
+++ code/swarna/services/auth_service.dart
@@ -1,23 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_expense_tracker/screens/home_screen.dart';
-
-
-
+/// Service class for handling authentication operations
 class AuthService{
-
-  createUser(data,context) async {
+/// Method to create a new user using Firebase Authentication
+  /// 
+  /// Takes `data` (a map containing 'email' and 'password') and `context` for navigation.
+  Future<void> createUser(Map<String, String> data, BuildContext context) async {
     try {
+       // Attempt to create a new user with email and password
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: data['email'],
         password: data['password'],
       );
+      // Navigate to HomeScreen upon successful signup
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) =>  const HomeScreen()),
       );
 
     } catch (e) {
+      // Show error dialog if sign-up fails
       showDialog(
         context: context,
         builder: (context) {return AlertDialog(
@@ -27,17 +30,23 @@ class AuthService{
         });
     }
   }
-  login(data,context) async{
+  /// Method to log in an existing user using Firebase Authentication
+  /// 
+  /// Takes `data` (a map containing 'email' and 'password') and `context` for navigation.
+  Future<void> login(Map<String, String> data, BuildContext context) async {
     try {
+      // Attempt to sign in the user with email and password
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: data['email'],
         password: data['password'],
       );
+      // Navigate to HomeScreen upon successful login
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) =>  const HomeScreen()),
       );
     } catch (e) {
+      // Show error dialog if login fails
       showDialog(
           context: context,
           builder: (context) {return AlertDialog(
